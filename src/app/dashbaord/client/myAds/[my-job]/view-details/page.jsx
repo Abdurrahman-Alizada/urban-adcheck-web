fee34652fce390ce7443c61e40fe8242ed234c75
muhammad aqib <muhammadaqibkhan001@gmail.com>
2024-12-10 11:49:05 -0800

    dashbaord new pages

---
+++ src/app/dashbaord/client/myAds/[my-job]/view-details/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ViewDetails() {
+  return (
+    <div className='w-ful mt-4 '>
+       
+       {/* navigation */}
+       <div>
+          <h2 className="text-gray-700 text-[20px] lg:text-[24px] font-extrabold font-Archivoo">
+             <span>1 .</span> Oliviya Rhye
+          </h2>
+       </div>
+
+    </div>
+  )
+}
+
+export default ViewDetails
