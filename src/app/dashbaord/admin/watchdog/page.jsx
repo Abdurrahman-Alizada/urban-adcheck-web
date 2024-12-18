b6c990653fee46d330095d5f3e007669cda2d714
muhammad aqib <muhammadaqibkhan001@gmail.com>
2024-12-17 17:01:55 -0800

    fixed dashboard issues

--- src/app/dashbaord/admin/watchdog/page.jsx
+++ src/app/dashbaord/admin/watchdog/page.jsx
@@ -49,7 +49,7 @@ function Watchdog() {
                                     </div> 
                               </div>
                               <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
                               standard dummy text ever since the 1500s, 
                               </p>  
                           </div>
@@ -82,7 +82,7 @@ function Watchdog() {
                                     </div> 
                               </div>
                               <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
                               standard dummy text ever since the 1500s, 
                               </p>
                           </div>
@@ -115,7 +115,7 @@ function Watchdog() {
                                     </div> 
                               </div>
                               <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
                               standard dummy text ever since the 1500s, 
                               </p>  
                           </div>
@@ -148,142 +148,11 @@ function Watchdog() {
                                     </div> 
                               </div>
                               <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
                               standard dummy text ever since the 1500s, 
                               </p>
                           </div>
-                          <div className='bg-white p-3 shadow-custom-shadow'>
-                              <div className='flex justify-between'>
-                                    <div className='flex items-start gap-3'>
-                                         <div>
-                                            <Image
-                                             src={'/watchdog-profile.png'}
-                                             width={85}
-                                             height={85}
-                                             alt={'watchdog profile'}
-                                             className="rounded-md"
-                                            />
-                                         </div> 
-                                         <div>
-                                            <h2 className="text-gray-700 text-[18px] lg:text-[24px] font-extrabold font-nunitosans">
-                                            Namehere
-                                            </h2>
-                                            <span className='text-[#9C9C9C] block font-NotoSans text-[12px] mb-1'>Jobs Completed : 15</span>
-                                            <span className='text-[#9C9C9C]  font-NotoSans text-[10.5px] flex items-center'><HiOutlineLocationMarker size={20} color='#068179'/>Baharia Town, Rawalpindi</span>
-                                         </div>
-                                    </div> 
-                                    <div>
-                                       <button
-                                        className="flex gap-2 items-center bg-transparent border-[1px] border-primary text-primary px-2 md:px-4 py-1 rounded-md"
-                                        >
-                                        View Profile
-                                        </button>     
-                                    </div> 
-                              </div>
-                              <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                              standard dummy text ever since the 1500s, 
-                              </p>  
-                          </div>
-                          <div className='bg-white p-2 shadow-custom-shadow'>
-                              <div className='flex justify-between'>
-                                    <div className='flex items-start gap-3'>
-                                         <div>
-                                            <Image
-                                             src={'/watchdog-profile.png'}
-                                             width={85}
-                                             height={85}
-                                             alt={'watchdog profile'}
-                                             className="rounded-md"
-                                            />
-                                         </div> 
-                                         <div>
-                                            <h2 className="text-gray-700 text-[18px] lg:text-[24px] font-extrabold font-nunitosans">
-                                            Namehere
-                                            </h2>
-                                            <span className='text-[#9C9C9C] block font-NotoSans text-[12px] mb-1'>Jobs Completed : 15</span>
-                                            <span className='text-[#9C9C9C]  font-NotoSans text-[10.5px] flex items-center'><HiOutlineLocationMarker size={20} color='#068179'/>Baharia Town, Rawalpindi</span>
-                                         </div>
-                                    </div> 
-                                    <div>
-                                       <button
-                                        className="flex gap-2 items-center bg-transparent border-[1px] border-primary text-primary px-2 md:px-4 py-1 rounded-md"
-                                        >
-                                        View Profile
-                                        </button>     
-                                    </div> 
-                              </div>
-                              <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                              standard dummy text ever since the 1500s, 
-                              </p>
-                          </div>
-                          <div className='bg-white p-3 shadow-custom-shadow'>
-                              <div className='flex justify-between'>
-                                    <div className='flex items-start gap-3'>
-                                         <div>
-                                            <Image
-                                             src={'/watchdog-profile.png'}
-                                             width={85}
-                                             height={85}
-                                             alt={'watchdog profile'}
-                                             className="rounded-md"
-                                            />
-                                         </div> 
-                                         <div>
-                                            <h2 className="text-gray-700 text-[18px] lg:text-[24px] font-extrabold font-nunitosans">
-                                            Namehere
-                                            </h2>
-                                            <span className='text-[#9C9C9C] block font-NotoSans text-[12px] mb-1'>Jobs Completed : 15</span>
-                                            <span className='text-[#9C9C9C]  font-NotoSans text-[10.5px] flex items-center'><HiOutlineLocationMarker size={20} color='#068179'/>Baharia Town, Rawalpindi</span>
-                                         </div>
-                                    </div> 
-                                    <div>
-                                       <button
-                                        className="flex gap-2 items-center bg-transparent border-[1px] border-primary text-primary px-2 md:px-4 py-1 rounded-md"
-                                        >
-                                        View Profile
-                                        </button>     
-                                    </div> 
-                              </div>
-                              <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                              standard dummy text ever since the 1500s, 
-                              </p>  
-                          </div>
-                          <div className='bg-white p-2 shadow-custom-shadow'>
-                              <div className='flex justify-between'>
-                                    <div className='flex items-start gap-3'>
-                                         <div>
-                                            <Image
-                                             src={'/watchdog-profile.png'}
-                                             width={85}
-                                             height={85}
-                                             alt={'watchdog profile'}
-                                             className="rounded-md"
-                                            />
-                                         </div> 
-                                         <div>
-                                            <h2 className="text-gray-700 text-[18px] lg:text-[24px] font-extrabold font-nunitosans">
-                                            Namehere
-                                            </h2>
-                                            <span className='text-[#9C9C9C] block font-NotoSans text-[12px] mb-1'>Jobs Completed : 15</span>
-                                            <span className='text-[#9C9C9C]  font-NotoSans text-[10.5px] flex items-center'><HiOutlineLocationMarker size={20} color='#068179'/>Baharia Town, Rawalpindi</span>
-                                         </div>
-                                    </div> 
-                                    <div>
-                                       <button
-                                        className="flex gap-2 items-center bg-transparent border-[1px] border-primary text-primary px-2 md:px-4 py-1 rounded-md"
-                                        >
-                                        View Profile
-                                        </button>     
-                                    </div> 
-                              </div>
-                              <p className='p-2  mt-2 text-[15.03px] font-nunitosans leading-[22px] from-neutral-400 text-[#767E94] '>
-                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                              standard dummy text ever since the 1500s, 
-                              </p>
-                          </div>
+                          
                    </div>
     </div>
   )
