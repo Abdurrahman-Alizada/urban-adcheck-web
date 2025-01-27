f145bbb8db69e102fb2b071b8acfe5b585c4158e
aqib4 <muhammadaqibkhan001@gmail.com>
2025-01-27 18:52:15 +0500

    pricing loader fixed

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,28 @@ import FAQ from "@/components/Faq/page";
 import CTA from "@/components/CTA/page";
 import Testimonials from "@/components/landingPage/TestimonialsSlider/page";
 import { useGetAllPackagesQuery } from "@/redux/reducers/package/packageThunk";
+import PackageLoader from "@/components/contentLoader/PackagesLoader/page";
 
 export default function Home() {
-       const {data,isLoading}=useGetAllPackagesQuery();
+       const {data,isLoading,isFetching}=useGetAllPackagesQuery();
   return (
     <div>
       <main >
            <Hero/>
            <Clients/>
-           <ProductPricing packages={data?.data} isLoading={isLoading}/>
+           <div className='mt-16 flex justify-center gap-2 mb-12'>
+        {  
+          (isLoading || isFetching) ? Array.from({ length: 2 }).map((_, index) => (              
+               <div className='flex' key={index}>
+               <PackageLoader  />
+              </div>
+              
+          ))
+          :
+         <ProductPricing packages={data?.data}/>  
+        }
+       
+    </div>
            <WhatYouCanDo/>
            <ContactHome/>
            <Testimonials/>
