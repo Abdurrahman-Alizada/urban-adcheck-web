ef7c1ae81696a10e45b86cf6aaeb46390aa80716
Abdurrahman-Alizada <63086457+Abdurrahman-Alizada@users.noreply.github.com>
2025-06-05 23:29:42 +0500

    base url

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,3 +1,3 @@
 // export const baseURL = 'https://urban-adcheck-backend.onrender.com/api/v1'
-export const baseURL = 'http://93.127.197.74:1337/api/v1'
+export const baseURL = 'https://dev-admin.qcsdrive.com/api/v1'
 
